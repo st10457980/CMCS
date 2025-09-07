ac01c3c64da0949e57219d4d70d4c7f99d60cc28
Kgotso <st10457980@rcconnect.edu.za>
2025-09-07 15:02:29 +0200

    Added UML class diagram and documentation for CMCS

--- obj/Debug/net9.0-windows/CMCS.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/CMCS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ea1e6b6c57efdc33d2aa9af2dc945a4bc988e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
